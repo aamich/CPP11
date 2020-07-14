7a4979b2af692625511b5bc62bd0e7bfc940bf0d
aamich <34666597+aamich@users.noreply.github.com>
2020-07-14 11:06:53 -0700

    Update trim.cpp

--- trim/trim.cpp
+++ trim/trim.cpp
@@ -8,3 +8,22 @@ inline std::string trim(const std::string &s)
    auto wsback=std::find_if_not(s.rbegin(),s.rend(),[](int c){return std::isspace(c);}).base();
    return (wsback<=wsfront ? std::string() : std::string(wsfront,wsback));
 }
+
+std::string trim(const std::string &s)
+{
+    std::string::const_iterator it = s.begin();
+    while (it != s.end() && isspace(*it))
+        it++;
+
+    std::string::const_reverse_iterator rit = s.rbegin();
+    while (rit.base() != it && isspace(*rit))
+        rit++;
+
+    return std::string(it, rit.base());
+}
+
+// boost
+#include <boost/algorithm/string.hpp>
+
+std::string str("hello world! ");
+boost::trim_right(str);
